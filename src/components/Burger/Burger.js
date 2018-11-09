160f135508ec764236fa64932a6fd65cb52eb172
Clark Newell <williamcnewell@gmail.com>
2018-11-08 21:12:10 -0700

    current stage deployed

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -15,7 +15,7 @@ const burger = ( props ) => {
             return arr.concat(el)
         }, []);
     if (transformedIngredients.length === 0) {
-        transformedIngredients = <p>Please start adding ingredients!</p>;
+        transformedIngredients = <p>Start adding ingredients!</p>;
     }
     return (
         <div className={classes.Burger}>
