e62138abe79701107ad4d1f1f84712e04c7fc905
Clark Newell <williamcnewell@gmail.com>
2018-11-12 11:39:53 -0700

    adding test to auth reducer

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -9,8 +9,28 @@ import NavigationItem from './NavigationItem/NavigationItem'
 configure ({ adapter: new Adapter() })
 
 describe('<NavigationItems />', () => {
+
+    let wrapper
+
+    beforeEach(() => {
+        wrapper = shallow( <NavigationItems /> )
+    })
+
     it('should render two <NavigationItem /> elements if not authenticated', () => {
-        const wrapper = shallow( <NavigationItems /> )
         expect(wrapper.find(NavigationItem)).toHaveLength(2)
     })
+
+    it('should render three <NavigationItem /> elements if authenticated', () => {
+        // wrapper = shallow( < NavigationItems isAuthenticated /> )
+
+        wrapper.setProps({ isAuthenticated: true })
+        expect(wrapper.find(NavigationItem)).toHaveLength(3)
+    })
+
+    it('should render the Log Out <NavigationItem /> element if authenticated', () => {
+
+        wrapper.setProps({ isAuthenticated: true })
+        expect(wrapper.contains( <NavigationItem link='/logout'>Log Out</NavigationItem> )).toEqual(true)
+    })
 })
+
