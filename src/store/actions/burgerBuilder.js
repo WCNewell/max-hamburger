91ba445394d18a5104765b20cb38f3de45c195d5
Clark Newell <williamcnewell@gmail.com>
2018-11-28 16:45:31 -0700

    adding bugerBuilder saga

--- src/store/actions/burgerBuilder.js
+++ src/store/actions/burgerBuilder.js
@@ -1,5 +1,5 @@
 import * as actionTypes from './actionTypes'
-import axios from '../../axios-orders'
+// import axios from '../../axios-orders'
 
 export const addIngredient = ( name ) => {
     return {
@@ -29,13 +29,17 @@ export const fetchIngredientsFailed = () => {
 }
 
 export const initIngredients = () => {
-    return dispatch => {
-        axios.get('/ingredients.json')
-            .then( response => {
-               dispatch(setIngredients(response.data))
-            } )
-            .catch( error => {
-                dispatch(fetchIngredientsFailed())
-            } )
+    return {
+        type: actionTypes.INIT_INGREDIENTS
     }
+
+    // return dispatch => {
+    //     axios.get('/ingredients.json')
+    //         .then( response => {
+    //            dispatch(setIngredients(response.data))
+    //         } )
+    //         .catch( error => {
+    //             dispatch(fetchIngredientsFailed())
+    //         } )
+    // }
 }
--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -2,11 +2,23 @@ import { takeEvery } from 'redux-saga/effects'
 
 import * as actionTypes from '../actions/actionTypes'
 
-import { logoutSaga, checkAuthTimeoutSaga, authUserSaga, authCheckStateSaga } from './auth'
+import {
+    logoutSaga,
+    checkAuthTimeoutSaga,
+    authUserSaga,
+    authCheckStateSaga
+} from './auth'
+
+import { initIngredientsSaga } from './burgerBuilder'
 
 export function* watchAuth() {
     yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga)
     yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga)
     yield takeEvery(actionTypes.AUTH_USER, authUserSaga)
     yield takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckStateSaga)
+}
+
+export function* watchBurgerBuilder() {
+    yield takeEvery(actionTypes.INIT_INGREDIENTS, initIngredientsSaga)
+
 }
